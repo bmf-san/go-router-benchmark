bdbcccca5034ffe0fd5c4ee79e847a70452f4880
lkeix <ai.kanaria.ai@gmail.com>
2022-12-18 18:52:51 +0900

    fix

--- benchmark_test.go
+++ benchmark_test.go
@@ -532,6 +532,43 @@ func BenchmarkPathParamBracketRoutes10Gorouter(b *testing.B) {
 	benchmark(b, pathParamBracketRoutes10, router)
 }
 
+// go-ozzo/ozzo-routing
+// https://github.com/go-ozzo/ozzo-routing
+func BenchmarkStaticRoutesRootOzzoRouting(b *testing.B) {
+	router := loadOzzoRouting(staticRoutesRoot)
+	benchmark(b, staticRoutesRoot, router)
+}
+
+func BenchmarkStaticRoutes1OzzoRouting(b *testing.B) {
+	router := loadOzzoRouting(staticRoutes1)
+	benchmark(b, staticRoutes1, router)
+}
+
+func BenchmarkStaticRoutes5OzzoRouting(b *testing.B) {
+	router := loadOzzoRouting(staticRoutes5)
+	benchmark(b, staticRoutes5, router)
+}
+
+func BenchmarkStaticRoutes10OzzoRouting(b *testing.B) {
+	router := loadOzzoRouting(staticRoutes10)
+	benchmark(b, staticRoutes10, router)
+}
+
+func BenchmarkPathParamInequalitySignRoutes1OzzoRouting(b *testing.B) {
+	router := loadOzzoRouting(pathParamInequalitySignRoutes1)
+	benchmark(b, pathParamInequalitySignRoutes1, router)
+}
+
+func BenchmarkPathParamInequalitySignRoutes5OzzoRouting(b *testing.B) {
+	router := loadOzzoRouting(pathParamInequalitySignRoutes5)
+	benchmark(b, pathParamInequalitySignRoutes5, router)
+}
+
+func BenchmarkPathParamInequalitySignRoutes10OzzoRouting(b *testing.B) {
+	router := loadOzzoRouting(pathParamInequalitySignRoutes10)
+	benchmark(b, pathParamInequalitySignRoutes10, router)
+}
+
 // n9te9 router
 // https://github.com/lkeix/techbook13-sample
 func BenchmarkStaticRoutesRootN9tE9Routing(b *testing.B) {
